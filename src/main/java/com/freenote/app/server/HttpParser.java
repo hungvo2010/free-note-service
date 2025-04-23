7300bd7f7f5510589f2a6f7760f0f61dab6d74ef
hungvo2010 <hungvochanhdp1@gmail.com>
2025-04-23 18:01:12 +0700

    update

---
+++ src/main/java/com/freenote/app/server/HttpParser.java
@@ -0,0 +1,10 @@
+package com.freenote.app.server;
+
+import java.io.InputStream;
+import java.net.http.HttpRequest;
+
+public class HttpParser {
+    public HttpRequest parse(InputStream inputStream){
+
+    }
+}
