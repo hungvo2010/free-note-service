d4d5321d721cf1bcb276e24a6dcb2139e3997a97
hungvo2010 <hungvochanhdp1@gmail.com>
2025-05-29 17:56:37 +0700

    feat(websocket): update base frame

--- src/main/java/com/freenote/app/server/frames/BaseFrame.java
+++ src/main/java/com/freenote/app/server/frames/BaseFrame.java
@@ -1,47 +1,89 @@
 package com.freenote.app.server.frames;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 import java.io.*;
 import java.util.BitSet;
 
-@AllArgsConstructor
-@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class BaseFrame implements Serializable, Externalizable {
+    private static BitSet defaultBitset;
     @Serial
     private static final long serialVersionUID = -2140098214102580912L;
-    private boolean fin;
-    private boolean rsv1;
-    private boolean rsv2;
-    private boolean rsv3;
-    private BitSet opcode;
-    private boolean mask;
-    private long payloadLength;
+    private boolean fin = false;
+    private boolean rsv1 = false;
+    private boolean rsv2 = false;
+    private boolean rsv3 = false;
+    private BitSet opcode = defaultBitset; // Using BitSet to represent opcode
+    private boolean mask = false;
+    private byte payloadLength;
     private byte[] maskingKey;
     private byte[] payloadData;
     private byte[] extensionData;
     private byte[] applicationData;
 
+    static {
+        var bitsets = new BitSet(4);
+        bitsets.set(0, true); // Set the first bit to true
+        bitsets.set(1, 4, false); // Set bits 1, 2, and 3 to false
+        defaultBitset = bitsets;
+    }
+
+    public BaseFrame(byte[] bytes) {
+        payloadLength = (byte) (bytes.length * 8);
+        payloadData = bytes;
+        maskingKey = new byte[0];
+        extensionData = new byte[0];
+        applicationData = bytes;
+    }
+
     @Override
     public void writeExternal(ObjectOutput out) throws IOException {
-        out.writeBoolean(fin);
-        out.writeBoolean(rsv1);
-        out.writeBoolean(rsv2);
-        out.writeBoolean(rsv3);
-        out.writeObject(opcode);
-        out.writeBoolean(mask);
-        out.writeLong(payloadLength);
-        out.writeObject(maskingKey);
+        BitSet frameBits = new BitSet(4);
+        frameBits.set(0, fin);
+        frameBits.set(1, rsv1);
+        frameBits.set(2, rsv2);
+        frameBits.set(3, rsv3);
+        for (int i = 0; i < 4; i++) {
+            frameBits.set(4 + i, opcode.get(i));
+        }
+        frameBits.set(8, mask);
+        boolean[] payloadBits = longToBitArray(payloadLength);
+        for (int i = 0; i < payloadBits.length; i++) {
+            frameBits.set(9 + i, payloadBits[i]);
+        }
+        frameBits.set(17, maskingKey != null && maskingKey.length > 0);
+
+
+//        out.writeBoolean(fin);
+//        out.writeBoolean(rsv1);
+//        out.writeBoolean(rsv2);
+//        out.writeBoolean(rsv3);
+//        out.writeByte(opcode.toByteArray()[0]); // Assuming opcode is a single byte
+//        out.writeBoolean(mask);
+//        out.writeLong(payloadLength);
+//        out.writeObject(maskingKey);
+//        out.writeObject(payloadData);
+        out.writeObject(frameBits);
+//        out.writeObject(maskingKey);
         out.writeObject(payloadData);
-        out.writeObject(extensionData);
-        out.writeObject(applicationData);
+//        out.writeObject(extensionData);
+//        out.writeObject(applicationData);
     }
 
     @Override
     public void readExternal(ObjectInput in) throws IOException {
-       fin = in.readBoolean();
+        fin = in.readBoolean();
+    }
+
+    public static boolean[] longToBitArray(byte value) {
+        boolean[] bits = new boolean[8];
+        for (int i = 0; i < 8; i++) {
+            bits[i] = (value & 1) == 1; // Check if the last bit is 1
+            value >>= 1; // Right-shift the bits
+        }
+        return bits;
     }
 }
--- src/main/java/com/freenote/app/server/handler/impl/MockHandler.java
+++ src/main/java/com/freenote/app/server/handler/impl/MockHandler.java
@@ -1,10 +1,12 @@
 package com.freenote.app.server.handler.impl;
 
+import com.freenote.app.server.frames.BaseFrame;
 import com.freenote.app.server.handler.URIHandler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.*;
+import java.util.BitSet;
 
 public class MockHandler implements URIHandler {
     private static final Logger log = LogManager.getLogger(MockHandler.class);
@@ -13,14 +15,18 @@ public class MockHandler implements URIHandler {
     public void handle(InputStream inputStream, OutputStream outputStream) {
         try {
             var reader = new BufferedReader(new InputStreamReader(inputStream));
-            log.info(outputStream.getClass().getCanonicalName());
-            log.info(inputStream.getClass().getCanonicalName());
-            String data;
             while (reader.ready()) {
-                data = reader.readLine();
+                byte[] data = new byte[1014];
+                inputStream.read(data);
+                var bitset = BitSet.valueOf(data);
+                for (int i = 0; i < 16; i++) {
+                    System.out.println(bitset.get(i));
+                }
                 log.info("Reading from input stream: {}", data);
-                outputStream.write((data + System.lineSeparator()).getBytes());
-                outputStream.flush();
+                var sampleFrame = new BaseFrame(data);
+                ObjectOutput objectOutput = new ObjectOutputStream(outputStream);
+                sampleFrame.writeExternal(objectOutput);
+                objectOutput.flush();
             }
         } catch (IOException e) {
             log.error("Error handling input stream", e);
