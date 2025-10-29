6478e6d8f2de09252167880a85a6d37a8d9d8e57
hungvo2010 <hungvochanhdp1@gmail.com>
2025-10-29 18:04:37 +0700

    Update logic to connect to same room websocket

---
+++ src/main/java/com/freenote/app/server/application/models/core/RoomManager.java
@@ -0,0 +1,45 @@
+package com.freenote.app.server.application.models.core;
+
+import com.freenote.annotations.Singleton;
+import com.freenote.app.server.application.models.request.DraftRequest;
+import com.freenote.app.server.connections.Connection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Singleton
+public class RoomManager {
+    private Map<String, Room> rooms;
+    private static RoomManager instance;
+
+    private RoomManager() {
+        rooms = new HashMap<>();
+    }
+
+    public static RoomManager getInstance() {
+        if (instance == null) {
+            synchronized (RoomManager.class) {
+                instance = new RoomManager();
+            }
+        }
+        return instance;
+    }
+
+    public void addConnection(String draftId, DraftRequest draftRequest) {
+        var requestMeta = draftRequest.getMetaData();
+        var connection = new Connection(
+                requestMeta.getSocket(),
+                requestMeta.getIpAddress(),
+                requestMeta.getPort()
+        );
+        var draftRoom = this.rooms.putIfAbsent(draftId, new Room());
+        if (draftRoom != null) {
+            draftRoom.addMember(connection);
+        }
+    }
+
+    public void broadcastToRoom(String draftId, DraftAction returnAction) {
+        var room = this.rooms.get(draftId);
+        room.broadCastMessage(returnAction);
+    }
+}
