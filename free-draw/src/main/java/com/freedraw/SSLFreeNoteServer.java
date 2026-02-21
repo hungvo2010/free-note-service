d55a8236a507b92c75fd5a1a3add1fd4a4df6806
hungvo2010 <hungvochanhdp1@gmail.com>
2026-02-21 09:06:23 +0700

    log number of available processors

--- free-draw/src/main/java/com/freedraw/SSLFreeNoteServer.java
+++ free-draw/src/main/java/com/freedraw/SSLFreeNoteServer.java
@@ -30,12 +30,12 @@ public class SSLFreeNoteServer {
 
     // Main entrypoint
     public static void main(String[] args) throws Exception {
-         int port = Integer.parseInt(System.getenv().getOrDefault("SSL_PORT", "8189"));
+        int port = Integer.parseInt(System.getenv().getOrDefault("SSL_PORT", "8189"));
         String keystorePath = System.getenv().getOrDefault("KEYSTORE_PATH", "keystore.p12");
         String keystorePassword = System.getenv().getOrDefault("KEYSTORE_PASSWORD", "changeit");
-        
+
         log.info("Starting SSL server with keystore: {}", keystorePath);
-        
+
         SSLFreeNoteServer server = new SSLFreeNoteServer(
                 port,
                 keystorePath,
